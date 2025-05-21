f5ff834120ff1f5e702516534e4071fe61fbffb8
sanghyunbbb <parksanghyun9999@gmail.com>
2025-05-21 17:18:46 +0900

    2025-05-21

---
+++ 07_jdbc/jdbc_lecture/src/test/java/junit/AssertionsTest.java
@@ -0,0 +1,28 @@
+package junit;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AssertionsTest {
+//
+//    @Test
+//    void assertTests() {
+//        // 두 값이 동일한지 확인
+//        assertEquals(4,2*3,"곱셈 결과 확인");
+
+
+    @Test
+    void assertTests() {
+        // 조건식이 true 인지 확인
+//        assertTrue(5==1,"곱셈 결과 확인");
+//        assertNotNull(new Object(), "객체가 null이 아님을 확인");
+//
+//    assertThrows(ArithmeticException.class, () -> {
+//        int result = 1/0;
+//    });
+//    assertEquals("/ by zero",exception.getMessage());
+    }
+
+
+}
